627f70ff2121d761af4b20a0585efa7a92b6c4fd
Jonathan Araul <jonathan.araul@gmail.com>
2013-12-13 12:15:15 -0430

    Story 01: Inicio de proyecto de prueba
    Archivos iniciales y creacion de tablas usando migraciones.

---
+++ app/database/migrations/2013_12_11_212450_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function($table){
+			$table->create();
+			$table->increments('id');
+			$table->string('email');
+			$table->string('real_name');
+			$table->string('password');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
